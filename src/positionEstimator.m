619a57196766d9310cab60520dc16d35c8f6c596
chiaralazza <79700771+chiaralazza@users.noreply.github.com>
2021-03-16 03:42:26 +0100

    SVM + distance average algorithm functions

--- src/positionEstimator.m
+++ src/positionEstimator.m
@@ -1,93 +1,116 @@
-function [x, y] = positionEstimator(test_data, model_params)
+function [x, y, newParameters] = positionEstimator(past_current_trial, modelParameters)
+function pred = svmPredict_nested(model, X)
+%SVMPREDICT returns a vector of predictions using a trained SVM model
+%(svmTrain). 
+%   pred = SVMPREDICT(model, X) returns a vector of predictions using a 
+%   trained SVM model (svmTrain). X is a mxn matrix where there each 
+%   example is a row. model is a svm model returned from svmTrain.
+%   predictions pred is a m x 1 column of predictions of {0, 1} values.
+%
 
-  % **********************************************************
-  %
-  % You can also use the following function header to keep your state
-  % from the last iteration
-  %
-  % function [x, y, newmodel_params] = positionEstimator(test_data, model_params)
-  %                 ^^^^^^^^^^^^^^^^^^
-  % Please note that this is optional. You can still use the old function
-  % declaration without returning new model parameters. 
-  %
-  % *********************************************************
+% Check if we are getting a column vector, if so, then assume that we only
+% need to do prediction for a single example
+if (size(X, 2) == 1)
+    % Examples should be in rows
+    X = X';
+end
 
-  % - test_data:
-  %     test_data(m).trialID
-  %         unique trial ID
-  %     test_data(m).startHandPos
-  %         2x1 vector giving the [x y] position of the hand at the start
-  %         of the trial
-  %     test_data(m).decodedHandPos
-  %         [2xN] vector giving the hand position estimated by your
-  %         algorithm during the previous iterations. In this case, N is 
-  %         the number of times your function has been called previously on
-  %         the same data sequence.
-  %     test_data(m).spikes(i,t) (m = trial id, i = neuron id, t = time)
-  %     in this case, t goes from 1 to the current time in steps of 20
-  %     Example:
-  %         Iteration 1 (t = 320):
-  %             test_data.trialID = 1;
-  %             test_data.startHandPos = [0; 0]
-  %             test_data.decodedHandPos = []
-  %             test_data.spikes = 98x320 matrix of spiking activity
-  %         Iteration 2 (t = 340):
-  %             test_data.trialID = 1;
-  %             test_data.startHandPos = [0; 0]
-  %             test_data.decodedHandPos = [2.3; 1.5]
-  %             test_data.spikes = 98x340 matrix of spiking activity
-  
-  
-  
-  % ... compute position at the given timestep.
-  
-  % Return Value:
-  
-  % - [x, y]:
-  %     current position of the hand
-  
-    spike_dist_window = 80; % Window of spike density mixture model
-    spike_dist_std    = 50; % Standard deviation of spike density mixture model
-    x = -spike_dist_window:1:spike_dist_window;
-    y = normpdf(x, 0, spike_dist_std);
+% Dataset 
+m = size(X, 1);
+p = zeros(m, 1);
+pred = zeros(m, 1);
 
-    n_neuron = 98;
-    
-    L = size(test_data.spikes, 2);
-    
-    spikeDist = zeros(n_neuron, L);
-    for i = 1:n_neuron
-        % Mixture model of spike distribution resembling localised (time-dependent) spike rate, binned at dt = 1 ms
-        for l = 1:L
-            if test_data.spikes(i, l) == 1
-                for j = -spike_dist_window:spike_dist_window
-                    if j+l > 0 && j+l < L
-                        spikeDist(i, l+j) = test_data.spikes(i, l+j) + y(j+spike_dist_window+1);
-                    end
-                end
-            end
+if strcmp(func2str(model.kernelFunction), 'linearKernel')
+    % We can use the weights and bias directly if working with the 
+    % linear kernel
+    p = X * model.w + model.b;
+elseif contains(func2str(model.kernelFunction), 'gaussianKernel')
+    % Vectorized RBF Kernel
+    % This is equivalent to computing the kernel on every pair of examples
+    X1 = sum(X.^2, 2);
+    X2 = sum(model.X.^2, 2)';
+    K = bsxfun(@plus, X1, bsxfun(@plus, X2, - 2 * X * model.X'));
+    K = model.kernelFunction(1, 0) .^ K;
+    K = bsxfun(@times, model.y', K);
+    K = bsxfun(@times, model.alphas', K);
+    p = sum(K, 2);
+else
+    % Other Non-linear kernel
+    for i = 1:m
+        prediction = 0;
+        for j = 1:size(model.X, 1)
+            prediction = prediction + ...
+                model.alphas(j) * model.y(j) * ...
+                model.kernelFunction(X(i,:)', model.X(j,:)');
+        end
+        p(i) = prediction + model.b;
+    end
+end
+
+% Convert predictions into 0 / 1
+pred(p >= 0) =  1;
+pred(p <  0) =  0;
+
+end
+
+
+%first let us find the angle by using the trained model
+newParameters = modelParameters; %update later
+group_size = 300; %how many ms of spikes to look
+spikes = mean(past_current_trial.spikes(:, 1:group_size), 2);
+num_classes = 8; %:(
+max_time = size(past_current_trial.spikes, 2); %we want to estimate this time
+combs = nchoosek(1:num_classes,2); %gives every possible 2-combination of angles 1 to 8 (There are 28 many)
+if max_time == 320  %means we don't have a class yet
+    prediction_vector = zeros(1,nchoosek(num_classes, 2)); %for each test instance (20*8) we wil come up with 28 predictions (as we trained 28 pairwise models). Here we save the decisions of each classifier for the corresponding row's trial
+    for pair =1:size(combs,1) %back to the combs (which was saving all pairs of 1 to 8)
+        class_a = combs(pair, 1); %take first element of combination
+        class_b = combs(pair, 2); %take second ...
+        pred = svmPredict_nested(modelParameters.model{class_a,class_b}, spikes); %now bring the model of a vs. b, and predict all the instances by using this
+        if pred == 0 %if pred is 0 it means this is class a
+            pred = class_a;
+        else
+            pred = class_b;
+        end
+        prediction_vector(pair) = pred; %the next column vas 'el', so save all this prediction vector as the next column of prediction_vector, so for each of the 160 test instance we have the result of a new a versus b classifier
+    end
+
+    decision = mode(prediction_vector); %take mode of every row, as a row is a test trial, and there are 28 votes, so mode is the maximally voted class and we can take it
+    newParameters.tune = decision;
+else
+    decision = modelParameters.tune;
+    if decision == 0
+       disp('This cannot be possible, the test instance should start from 320 ms.')    
+    end
+end
+%now we know that this test instance is an angle of decision
+xs = [];
+ys = [];
+for train_trial = 1:size(modelParameters.olddata, 1) %
+    position_trial = modelParameters.olddata(train_trial, decision).handPos(1:2, :);
+    if (size(position_trial, 2) >= max_time) && norm(modelParameters.olddata(train_trial, decision).handPos(1:2,1) - past_current_trial.startHandPos(1:2,1)) <= 5 %optimise this 5
+       xs = [xs, position_trial(1, max_time)];
+       ys = [ys, position_trial(2, max_time)];
+    end
+end
+%you may delete later
+if size(xs,2) == 0
+    for train_trial = 1:size(modelParameters.olddata, 1) %
+        position_trial = modelParameters.olddata(train_trial, decision).handPos(1:2, :);
+        if (size(position_trial, 2) >= max_time)
+           xs = [xs, position_trial(1, max_time)];
+           ys = [ys, position_trial(2, max_time)];
         end
     end
-    
-    firing_rate = squeeze(spikeDist)'; % L x 98
-    
-    % Using current average spikes attempt to classify which trajecotry
-    for n = 1: n_neuron
-        for l = 1:L
-            if firing_rate(l,n) == 0
-                firing_rate(l,n) = 0.01;
-            end
-        end
-    end
-    mdl = model_params.mdl;
-    label_pred = predict(mdl, firing_rate);
-    
-    beta = model_params.beta; % 8 x 98 x 2
-    traj_pred = round(mean(label_pred));
-    
-    beta_label = beta(traj_pred, :, :); % 1 x 98 x 2
-    final_pred = firing_rate(end,:) * squeeze(beta_label); % 1 x 2
-    x = final_pred(1,1);
-    y = final_pred(1,2);
-    disp(final_pred);
+end
+%delete later
+
+if size(xs,2) >= 1
+   x = mean(xs);
+   y = mean(ys);
+elseif size(xs,2) == 0
+    x = past_current_trial.decodedHandPos(1,end); %previous decision
+    y = past_current_trial.decodedHandPos(2,end); 
+end
+
 end
--- src/positionEstimatorTraining.m
+++ src/positionEstimatorTraining.m
@@ -1,180 +1,251 @@
-function [model_params] = positionEstimatorTraining(training_data)
-    % Arguments:
-    %
-    % - training_data:
-    %     training_data(n,k)              (n = trial id,  k = reaching angle)
-    %     training_data(n,k).trialId      unique number of the trial
-    %     training_data(n,k).spikes(i,t)  (i = neuron id, t = time)
-    %     training_data(n,k).handPos(d,t) (d = dimension [1-3], t = time)
-    %
-    % ... train your model
-    %
-    % Return Value:
-    %
-    % - model_params:
-    %     single structure containing all the learned parameters of your
-    %     model and which can be used by the "positionEstimator" function.
-    
-    % Constants
-    spike_dist_window = 80; % Window of spike density mixture model
-    spike_dist_std    = 50; % Standard deviation of spike density mixture model
-    k_num_neighbours  = 7;  % Number of k-nearest neighbours used for KNN classification
-    
-    n_trials = size(training_data, 1);              % Number of recorded trials
-    n_trjs   = size(training_data, 2);              % Number of recorded trajectories
-    n_neuron = size(training_data(1,1).spikes, 1);  % Number of neurons
-    
-    % plot all trials with all trajectories
-    %plot_trajectories(training_data)
+function [modelParameters] = positionEstimatorTraining(trial)
+function sim = linearKernel(x1, x2)
+%LINEARKERNEL returns a linear kernel between x1 and x2
+%   sim = linearKernel(x1, x2) returns a linear kernel between x1 and x2
+%   and returns the value in sim
 
-    % find the length of the longest trial
-    L = 0;
-    fprintf("\nFinding the length of longest trial... ");
-    t0 = tic; % Begin ticker
-    for k = 1:n_trjs
-        for n = 1:n_trials    
-            l = length(training_data(n,k).spikes(1,:));
-            if l > L
-                L = l;
-            end
-        end
-    end
-    fprintf("Done (%.2f s).\n", toc(t0));
-    
-    % make all trajectories the same length
-    fprintf("Making all trajectories the same length (%d pts)... ", L);
-    t0 = tic; % Begin ticker
-    for k = 1:n_trjs
-        for n = 1:n_trials
-            for j = length(training_data(n, k).spikes(1, :)) + 1:L % adjust to max length range
-                training_data(n, k).handPos = [training_data(n, k).handPos ...
-                                               training_data(n, k).handPos(:, end)];
-                training_data(n, k).spikes  = [training_data(n, k).spikes ...
-                                               training_data(n, k).spikes(:, end)];
-            end
-        end
-    end
-    fprintf("Done (%.2f s).\n", toc(t0));
-    
-    % calculate the average trajectory
-    avg_trjs(n_trjs).handPos = [];
-    fprintf("Calculating average trajectories... ");
-    t0 = tic; % Begin ticker
-    for k = 1:n_trjs
-        trj = zeros(2, L);
-        for n = 1:n_trials
-            for j = 1:length(training_data(n, k).handPos(1, :))
-                trj(:, j) = trj(:, j) + training_data(n, k).handPos(1:2, j);
-            end
-        end
-        
-        avg_trjs(k).handPos = trj(:, :) / n_trials;
-    end
-    fprintf("Done (%.2f s).\n", toc(t0));
+% Ensure that x1 and x2 are column vectors
+x1 = x1(:); x2 = x2(:);
 
-    % plot average trajectory
-    %plot_avg_trajectories(avg_trjs)
-    
-    % Collect spike rate (density) function for each trial
-    fprintf("Collecting spike density function for each trial... ");
-    t0 = tic; % Begin ticker
-    x = -spike_dist_window:1:spike_dist_window;
-    y = normpdf(x, 0, spike_dist_std);
-    for n = 1:n_trials
-        for k = 1:n_trjs
-            training_data(n, k).spikeDist = zeros(n_neuron, L);
-            for i = 1:n_neuron
-                % Mixture model of spike distribution resembling localised (time-dependent) spike rate, binned at dt = 1 ms
-                for l = 1:L
-                    if training_data(n, k).spikes(i, l) == 1
-                        for j = -spike_dist_window:spike_dist_window
-                            if j+l > 0 && j+l < L
-                                training_data(n, k).spikeDist(i, l+j) = training_data(n, k).spikes(i, l+j) ...
-                                                                      + y(j+spike_dist_window+1);
-                            end
-                        end
-                    end
-                end
-            end
-        end
-    end
-    fprintf("Done (%.2f s).\n", toc(t0));
-    
-    % Average spike rate (density) across all trials for each neuron
-    avg_spike_rate = zeros(n_trjs, n_neuron, L);
-    fprintf("Calculating average spike rate across all trials for each neuron... ");
-    t0 = tic; % Begin ticker
-    for k = 1:n_trjs
-        for i = 1:n_neuron
-            for t = 1:L
-                spike_sum = 0;
-                for n = 1:n_trials
-                    spike_sum = spike_sum + training_data(n, k).spikeDist(i, t);
-                end
-                avg_spike_rate(k, i, t) = spike_sum / n_trials;
-            end
-        end
-    end
-    fprintf("Done (%.2f s).\n", toc(t0));
-    
-    % Linear regression between spike densities and average trajectories
-    fprintf("Begin calculation of linear regression weights... ");
-    t0 = tic; % Begin ticker
-    pos_preds = []; % 7800 x 2
-    beta = zeros(n_trjs, n_neuron, 2); % 8 x 98 x 2
-    
-    features = []; % 98 x 7800
-    labels = []; % 1 x 7800
-    for k = 1:n_trjs
-        pos = [avg_trjs(k).handPos(1, :); avg_trjs(k).handPos(2, :)]'; % 975 x 2
-        firing_rate = [squeeze(avg_spike_rate(k, :, :))]; %  98 x 975
-        
-        features = [features , firing_rate];
-        these_labels = ones(1, L) * k;
-        labels = [labels , these_labels];
-        
-        beta_now = lsqminnorm(firing_rate', pos); % 98 x 2
-                        
-        beta(k, :, :) = beta_now;
-                
-        pos_pred = firing_rate' * beta_now; % 975 x 2
-        pos_preds = [pos_preds; pos_pred]; % 7800 x 2
-    end
-    fprintf("Done (%.2f s).\n", toc(t0));
-        
-    model_params.beta = beta;
-    
-    % Plot prediction
-    for k = 1:n_trjs
-        
-        end_point = k * L;
-        
-        start_point = (k-1) * L + 1;
-        pos_current = pos_preds(start_point:end_point, :);
+% Compute the kernel
+sim = x1' * x2;  % dot product
 
-        x_pos = pos_current(:, 1);
-        y_pos = pos_current(:, 2);
+end
+function sim = gaussianKernel(x1, x2, sigma)
+%RBFKERNEL returns a radial basis function kernel between x1 and x2
+%   sim = gaussianKernel(x1, x2) returns a gaussian kernel between x1 and x2
+%   and returns the value in sim
 
-        plot(x_pos, y_pos, 'r')
-        
-        hold on
-    end
-    axis square
+% Ensure that x1 and x2 are column vectors
+x1 = x1(:); x2 = x2(:);
+
+sim = exp(-(norm(x1 - x2) ^ 2) / (2 * (sigma ^ 2)));
     
-    features = features'; % 7800 x 98
-    labels = labels';
-    Mdl = fitcknn(features,labels, 'NumNeighbors', k_num_neighbours);
-    model_params.mdl = Mdl;
-    
-    accuracy = 0;
-    for i = 1:L*n_trjs
-        x_in = features(i,:);
-        prediction = predict(Mdl, x_in);
-        if prediction == labels(i)
-            accuracy = accuracy + 1;
+end
+
+function [model] = svmTrain_nested(X, Y, C, kernelFunction, ...
+                            tol, max_passes) %copying the whole svm function
+%SVMTRAIN Trains an SVM classifier using a simplified version of the SMO 
+%algorithm. 
+%   [model] = SVMTRAIN(X, Y, C, kernelFunction, tol, max_passes) trains an
+%   SVM classifier and returns trained model. X is the matrix of training 
+%   examples.  Each row is a training example, and the jth column holds the 
+%   jth feature.  Y is a column matrix containing 1 for positive examples 
+%   and 0 for negative examples.  C is the standard SVM regularization 
+%   parameter.  tol is a tolerance value used for determining equality of 
+%   floating point numbers. max_passes controls the number of iterations
+%   over the dataset (without changes to alpha) before the algorithm quits.
+%
+% Note: This is a simplified version of the SMO algorithm for training
+%       SVMs. In practice, if you want to train an SVM classifier, we
+%       recommend using an optimized package such as:  
+%
+%           LIBSVM   (http://www.csie.ntu.edu.tw/~cjlin/libsvm/)
+%           SVMLight (http://svmlight.joachims.org/)
+%
+%
+
+if ~exist('tol', 'var') || isempty(tol)
+    tol = 1e-3;
+end
+
+if ~exist('max_passes', 'var') || isempty(max_passes)
+    max_passes = 5;
+end
+
+% Data parameters
+m = size(X, 1);
+n = size(X, 2);
+
+% Map 0 to -1
+Y(Y==0) = -1;
+
+% Variables
+alphas = zeros(m, 1);
+b = 0;
+E = zeros(m, 1);
+passes = 0;
+eta = 0;
+L = 0;
+H = 0;
+
+% Pre-compute the Kernel Matrix since our dataset is small
+% (in practice, optimized SVM packages that handle large datasets
+%  gracefully will _not_ do this)
+% 
+% We have implemented optimized vectorized version of the Kernels here so
+% that the svm training will run faster.
+if strcmp(func2str(kernelFunction), 'linearKernel')
+    % Vectorized computation for the Linear Kernel
+    % This is equivalent to computing the kernel on every pair of examples
+    K = X*X';
+elseif contains(func2str(kernelFunction), 'gaussianKernel')
+    % Vectorized RBF Kernel
+    % This is equivalent to computing the kernel on every pair of examples
+    X2 = sum(X.^2, 2);
+    K = bsxfun(@plus, X2, bsxfun(@plus, X2', - 2 * (X * X')));
+    K = kernelFunction(1, 0) .^ K;
+else
+    % Pre-compute the Kernel Matrix
+    % The following can be slow due to the lack of vectorization
+    K = zeros(m);
+    for i = 1:m
+        for j = i:m
+             K(i,j) = kernelFunction(X(i,:)', X(j,:)');
+             K(j,i) = K(i,j); %the matrix is symmetric
         end
     end
-    disp(accuracy/(L*n_trjs));
-    whos
-    figure
+end
+
+% Train
+fprintf('\nTraining ...');
+dots = 12;
+while passes < max_passes
+            
+    num_changed_alphas = 0;
+    for i = 1:m
+        
+        % Calculate Ei = f(x(i)) - y(i) using (2). 
+        % E(i) = b + sum (X(i, :) * (repmat(alphas.*Y,1,n).*X)') - Y(i);
+        E(i) = b + sum (alphas.*Y.*K(:,i)) - Y(i);
+        
+        if ((Y(i)*E(i) < -tol && alphas(i) < C) || (Y(i)*E(i) > tol && alphas(i) > 0))
+            
+            % In practice, there are many heuristics one can use to select
+            % the i and j. In this simplified code, we select them randomly.
+            j = ceil(m * rand());
+            while j == i  % Make sure i \neq j
+                j = ceil(m * rand());
+            end
+
+            % Calculate Ej = f(x(j)) - y(j) using (2).
+            E(j) = b + sum (alphas.*Y.*K(:,j)) - Y(j);
+
+            % Save old alphas
+            alpha_i_old = alphas(i);
+            alpha_j_old = alphas(j);
+            
+            % Compute L and H by (10) or (11). 
+            if (Y(i) == Y(j))
+                L = max(0, alphas(j) + alphas(i) - C);
+                H = min(C, alphas(j) + alphas(i));
+            else
+                L = max(0, alphas(j) - alphas(i));
+                H = min(C, C + alphas(j) - alphas(i));
+            end
+           
+            if (L == H)
+                % continue to next i. 
+                continue;
+            end
+
+            % Compute eta by (14).
+            eta = 2 * K(i,j) - K(i,i) - K(j,j);
+            if (eta >= 0),
+                % continue to next i. 
+                continue;
+            end
+            
+            % Compute and clip new value for alpha j using (12) and (15).
+            alphas(j) = alphas(j) - (Y(j) * (E(i) - E(j))) / eta;
+            
+            % Clip
+            alphas(j) = min (H, alphas(j));
+            alphas(j) = max (L, alphas(j));
+            
+            % Check if change in alpha is significant
+            if (abs(alphas(j) - alpha_j_old) < tol)
+                % continue to next i. 
+                % replace anyway
+                alphas(j) = alpha_j_old;
+                continue;
+            end
+            
+            % Determine value for alpha i using (16). 
+            alphas(i) = alphas(i) + Y(i)*Y(j)*(alpha_j_old - alphas(j));
+            
+            % Compute b1 and b2 using (17) and (18) respectively. 
+            b1 = b - E(i) ...
+                 - Y(i) * (alphas(i) - alpha_i_old) *  K(i,j)' ...
+                 - Y(j) * (alphas(j) - alpha_j_old) *  K(i,j)';
+            b2 = b - E(j) ...
+                 - Y(i) * (alphas(i) - alpha_i_old) *  K(i,j)' ...
+                 - Y(j) * (alphas(j) - alpha_j_old) *  K(j,j)';
+
+            % Compute b by (19). 
+            if (0 < alphas(i) && alphas(i) < C)
+                b = b1;
+            elseif (0 < alphas(j) && alphas(j) < C)
+                b = b2;
+            else
+                b = (b1+b2)/2;
+            end
+
+            num_changed_alphas = num_changed_alphas + 1;
+
+        end
+        
+    end
+    
+    if (num_changed_alphas == 0)
+        passes = passes + 1;
+    else
+        passes = 0;
+    end
+
+    %fprintf('.');
+    dots = dots + 1;
+    if dots > 78
+        dots = 0;
+        %fprintf('\n');
+    end
+    if exist('OCTAVE_VERSION')
+        fflush(stdout);
+    end
+end
+fprintf(' Done! \n\n');
+
+% Save the model
+idx = alphas > 0;
+model.X= X(idx,:);
+model.y= Y(idx);
+model.kernelFunction = kernelFunction;
+model.b= b;
+model.alphas= alphas(idx);
+model.w = ((alphas.*Y)'*X)';
+
+end
+
+train = {}; %train{1} will keep the spike data (that is averaged and times are gone) over angle 1 trials/instances
+[num_trials , num_classes]= size(trial); %keep size 80,8
+nr_neurons = size(trial(1,1).spikes,1); %98, this is number of rows in .spike data
+group_size = 300; %we will average these many milliseconds to only keep neuron information
+for class = 1:num_classes %for everz class do the following
+   spikes = zeros(num_trials, nr_neurons); %a classes dataset will have 100 ...
+   %rows for each trial and 98 variables for each neuron
+   for t = 1:num_trials %for 80 instances, fill the spike rows (spike's row is a trial that averages over time)
+       spikes(t, :) = mean(trial(t,class).spikes(:, 1:group_size), 2); %corresponding class dataset's t'th instance's 1:300 times are averaged for each neuron
+   end
+   train{class} = spikes; %save spikes as this angle's dataset (spikes will be overriden next)
+end
+
+%train all combos
+models = {}; %for example models{4,7} will keep the SVM that is trained to figure out 4's versus 7's by using only train{4} and train{7}
+times = []; %28 many SVMs', for each we will keep track of the time to train
+combs = nchoosek(1:num_classes,2); %gives every possible 2-combination of angles 1 to 8 (There are 28 many)
+for pair =1:size(combs,1) %for all pair size
+    class_a = combs(pair, 1); %take this pair's first elements
+    class_b = combs(pair, 2); %take this pair's second elements
+    X = [train{class_a}; train{class_b}]; %we should only train on class a and class b instances (80*2 rows and 98 columns because a column is a neuron)
+    tic %start counting
+    [model] = svmTrain_nested(X, repelem(0:1, size(trial,1))', 20, @linearKernel, 0.01, 500); %train SVM. First input is the predictors. Second is true angles that we write, 20 is penalty that we tuned, linear kernel, 0.001 error tolerance that is default, 10,000 iterations over dataset to train svm
+    times = [times, toc]; %append the time
+    models{class_a,class_b} = model; %save the trained SVM to the pair's index in models cell
+end
+modelParameters.model = models; %send it to the test function
+%now all SVMs are trained
+% now time to keep all the hand positions to use
+modelParameters.olddata = trial; %use this later to compute location data
+modelParameters.runtime = sum(times);
+modelParameters.tune = 0; %we will tune this later
 end
